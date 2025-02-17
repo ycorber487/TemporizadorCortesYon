9502fbe966089b487585baf0761323b81b0c3a2b
ycorber487 <163828691+ycorber487@users.noreply.github.com>
2025-02-17 09:13:06 +0100

    Delete Temporizador_CortesYon directory

--- Temporizador_CortesYon/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ies.montecillos.temporizador_cortesyon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ies.montecillos.temporizador_cortesyon to javafx.fxml;
-    exports ies.montecillos.temporizador_cortesyon;
-}
